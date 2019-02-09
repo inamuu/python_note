68965ed4e9568db318bc26c5670a724d8dcda31a
inamuu <8310973+inamuu@users.noreply.github.com>
2019-02-09 19:09:47 +0900

    :elephant: ファイルがあったら削除するように

--- task-runner/tasks.py
+++ task-runner/tasks.py
@@ -11,7 +11,6 @@ def createfile(c):
     for name in list:
       if c.run(f"test -e {name}.txt", warn=True).ok:
         c.run(f"rm -f {name}.txt")
-    c.run("ls -la")
-    for name in list:
+        c.run(f"echo remove {name}.txt")
       c.run(f"touch {name}.txt")
     c.run("ls -la")
