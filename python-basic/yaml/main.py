f3134d9355e5ed01b4da3a4dcc90540886b2dc77
inamuu <8310973+inamuu@users.noreply.github.com>
2019-04-14 01:34:44 +0900

    :cop: loadだとunsafeだよとおこられるので修正＆引数取る
    https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation

--- python-basic/yaml/main.py
+++ python-basic/yaml/main.py
@@ -1,10 +1,13 @@
 # coding: utf-8
 
 import yaml
+import sys
 
 def main():
     with open('test.yaml') as file:
-        yml = yaml.load(file)
+        yml = yaml.safe_load(file)
         print(yml)
+    args = sys.argv
+    print(args)
 
 if __name__ == '__main__': main()
