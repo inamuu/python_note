b7895fe4d9ec1fdde6ffd59b3b2bbf56ebcf8dd7
inamuu <8310973+inamuu@users.noreply.github.com>
2019-04-14 00:45:59 +0900

    :hammer: パスを移動しておく＆先頭にutf-8を書かないと日本語で死ぬのを対応

---
+++ python-basic/argv/main.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+import sys
+
+def help():
+    command_name = sys.argv[0]
+    helpmsg = """
+  ヘルプだよ
+  引数を1つだけ指定してね
+"""
+    print(helpmsg)
+
+def checkarg():
+    if len(sys.argv) == 2:
+      args = sys.argv[1]
+      return args
+    else:
+      help()
+      exit()
+
+def main():
+    msg = checkarg()
+    print(msg)
+
+if __name__ == "__main__": main()
