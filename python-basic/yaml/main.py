434a90d5b5dfd570695c5d8cffa46136b6015dcc
inamuu <8310973+inamuu@users.noreply.github.com>
2019-04-14 21:31:35 +0900

    :+1: yamlが読めるところまで

--- python-basic/yaml/main.py
+++ python-basic/yaml/main.py
@@ -5,9 +5,15 @@ import sys
 
 def main():
     with open('test.yaml') as file:
-        yml = yaml.safe_load(file)
-        print(yml)
-    args = sys.argv
-    print(args)
+        yml = yaml.full_load(file)
+        
+    args = sys.argv[1]
+    val = yml[args]
+    name = yml[args]['name']
+    age = yml[args]['age']
+    print(val)
+    print(name)
+    print(age)
+
 
 if __name__ == '__main__': main()
