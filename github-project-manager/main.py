b94371fef92afb24a36533107e2c6747e3f54b02
inamuu <8310973+inamuu@users.noreply.github.com>
2019-04-25 02:47:43 +0900

    :+1: コミット

--- github-project-manager/main.py
+++ github-project-manager/main.py
@@ -1,8 +1,12 @@
 # coding: utf-8
 
 import sys
+from github import Github
 
 def main():
-    print('hello world')
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    g = Github(os.environ.get("github_access_token"))
+    print(g)
 
 if __name__ == '__main__': main()
