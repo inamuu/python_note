16fa5099dd19b4adbf82929d3d060932f3764199
inamuu <8310973+inamuu@users.noreply.github.com>
2019-04-25 02:54:14 +0900

    :+1: リポジトリ一覧を取得

--- github-project-manager/main.py
+++ github-project-manager/main.py
@@ -9,6 +9,8 @@ def main():
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
     g = Github(os.environ.get("github_access_token"))
-    print(g)
+
+    for repo in g.get_user().get_repos():
+        print(repo.name)
 
 if __name__ == '__main__': main()
