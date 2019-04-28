2caeabd7af5dd99265eabe6ab23ece3b4448c277
inamuu <8310973+inamuu@users.noreply.github.com>
2019-04-29 00:11:13 +0900

    :+1: 複数文字列に対応

--- python-basic/yesno/yesno.py
+++ python-basic/yesno/yesno.py
@@ -3,11 +3,11 @@
 def main():
     while True:
         choice = raw_input('please input [y/n] ')
-        if choice == 'y':
+        if choice in ['y', 'yes']:
             print('yes!')
             return True
-        elif choice == 'n':
+        elif choice in ['n', 'no']:
             print('No!')
-            return True
+            return False
 
 if __name__ == '__main__': main()
