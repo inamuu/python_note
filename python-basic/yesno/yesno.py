091e3188db2aacfdec89c6833d6beea0683699da
inamuu <8310973+inamuu@users.noreply.github.com>
2019-04-28 23:57:25 +0900

    :bug: 型チェック

--- python-basic/yesno/yesno.py
+++ python-basic/yesno/yesno.py
@@ -3,9 +3,9 @@
 def main():
     while True:
         choice = raw_input('please input [y/n]')
-        if choice = 'y':
+        if choice == 'y':
             print('yes')
-        elif choice = 'n':
+        elif choice == 'n':
             print('no')
 
 if __name__ == '__main__': main():
