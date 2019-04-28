8569a919459669c92e7aa574878920f183d8bc42
inamuu <8310973+inamuu@users.noreply.github.com>
2019-04-29 00:07:22 +0900

    :+1: 体裁を整えた

--- python-basic/yesno/yesno.py
+++ python-basic/yesno/yesno.py
@@ -2,12 +2,12 @@
 
 def main():
     while True:
-        choice = raw_input('please input [y/n]')
+        choice = raw_input('please input [y/n] ')
         if choice == 'y':
-            print('yes')
+            print('yes!')
             return True
         elif choice == 'n':
-            print('no')
+            print('No!')
             return True
 
 if __name__ == '__main__': main()
