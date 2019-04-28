6ae9a57a35a8c442d5303bcfcb49463862d36d97
inamuu <8310973+inamuu@users.noreply.github.com>
2019-04-28 23:56:14 +0900

    :+1: 一旦コミット

---
+++ python-basic/yesno/yesno.py
@@ -0,0 +1,11 @@
+# coding=utf8
+
+def main():
+    while True:
+        choice = raw_input('please input [y/n]')
+        if choice = 'y':
+            print('yes')
+        elif choice = 'n':
+            print('no')
+
+if __name__ == '__main__': main():
