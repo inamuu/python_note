159c9c9267bc0ee5ef260092d5191c08075cfd88
inamuu <8310973+inamuu@users.noreply.github.com>
2019-04-28 23:59:27 +0900

    :+1: 返答したら終了するように

--- python-basic/yesno/yesno.py
+++ python-basic/yesno/yesno.py
@@ -5,7 +5,9 @@ def main():
         choice = raw_input('please input [y/n]')
         if choice == 'y':
             print('yes')
+            return True
         elif choice == 'n':
             print('no')
+            return True
 
 if __name__ == '__main__': main()
