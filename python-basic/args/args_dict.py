78bb15f588b49087e2ebeb7cec86b953ac49d6bd
inamuu <8310973+inamuu@users.noreply.github.com>
2019-04-29 15:56:06 +0900

    :+1: 辞書からの抽出は 辞書名['key']

--- python-basic/args/args_dict.py
+++ python-basic/args/args_dict.py
@@ -2,7 +2,11 @@
 
 def main(args):
     print(args)
+    for key, value in args.items():
+        print("%s: %s" % (key, value))
+    print(args['id'])
+    print(args['name'])
 
 if __name__ == '__main__':
-    args = "{ 'id': '01', 'name': 'taro' }"
+    args = { 'id': '01', 'name': 'taro' }
     main(args)
