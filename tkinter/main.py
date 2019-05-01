f69460b90f3f9b3eab658a81f5005190c3917076
inamuu <8310973+inamuu@users.noreply.github.com>
2019-05-01 21:51:15 +0900

    :+1: ボタンでsystemコマンドを実行できるように

--- tkinter/main.py
+++ tkinter/main.py
@@ -2,11 +2,20 @@
 
 import sys
 import tkinter as tk
+import subprocess
+
+def helloworld():
+    command = ['ls', '-l']
+    subprocess.call(command)
 
 def main():
     root = tk.Tk()
     root.title(u"Test Tools")
     root.geometry("400x300")
+
+    button = tk.Button(root, text="Dockerrun", command=helloworld)
+    button.grid()
+
     root.mainloop()
 
 if __name__ == '__main__': main()
