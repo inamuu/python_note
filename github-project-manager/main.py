69a650bee28dd595e7683794a16a67256d39067c
inamuu <8310973+inamuu@users.noreply.github.com>
2019-05-01 17:49:11 +0900

    :+1: 関数にわけた

--- github-project-manager/main.py
+++ github-project-manager/main.py
@@ -5,12 +5,16 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 from github import Github
 
-def main():
-    dotenv_path = join(dirname(__file__), '.env')
-    load_dotenv(dotenv_path)
-    g = Github(os.environ.get("github_access_token"))
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+g = Github(os.environ.get("github_access_token"))
 
+def repos():
     for repo in g.get_user().get_repos():
         print(repo.name)
+
+def main():
+
+    repos()
 
 if __name__ == '__main__': main()
