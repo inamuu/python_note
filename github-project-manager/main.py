d850d9d949558a45da9b866f63942c70fb633ee5
inamuu <8310973+inamuu@users.noreply.github.com>
2019-08-08 21:22:57 +0900

    :hammer: clickでサブコマンド化

--- github-project-manager/main.py
+++ github-project-manager/main.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 import os, sys
+import click
 from os.path import join, dirname
 from dotenv import load_dotenv
 from github import Github
@@ -9,23 +10,21 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 g = Github(os.environ.get("github_access_token"))
 
+@click.group()
+def subcommand():
+    pass
+
+@subcommand.command(help='リポジトリ一覧')
 def repos():
     for repo in g.get_user().get_repos():
         print(repo.name)
 
+@subcommand.command(help='オーガニゼーション')
 def orgsrepos():
     for repo in g.get_organization(os.environ.get("github_orgs")).get_repos():
         print(repo.name)
 
-def main(args):
-    if args == 'repos':
-        repos()
-    elif args == 'orgsrepos':
-        orgsrepos()
-    else:
-        print('そんな関数ないよ')
-        return
+def main():
+    subcommand()
 
-if __name__ == '__main__':
-    args = sys.argv[1]
-    main(args)
+if __name__ == '__main__': main()
