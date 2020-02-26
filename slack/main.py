722dac69c24b6f0567e61270483429c2d04e4613
inamuu <8310973+inamuu@users.noreply.github.com>
2020-02-26 12:27:45 +0900

    :+1: implement simple test notify

--- slack/main.py
+++ slack/main.py
@@ -1,13 +1,12 @@
 # coding: utf-8
 
+import os
 import slackweb
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-slack_webhook_url = os.environ.get("slack_webhook_url")
+slack_webhook_url = os.environ["slack_webhook_url"]
 
 def main():
-    slack = slackweb.Slack(url="コピーしたWebhookのURL")
-    slack.notify(text="pythonからslackさんへ")
+    slack = slackweb.Slack(url=slack_webhook_url)
+    slack.notify(text="This is test notify")
 
 if __name__ == '__main__': main()
