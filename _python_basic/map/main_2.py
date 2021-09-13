ce8c80e12c6558ff9a691f82ea7be3a59bb7b3ba
inamuu <8310973+inamuu@users.noreply.github.com>
2021-09-13 14:27:26 +0900

    add a map

--- _python_basic/map/main_2.py
+++ _python_basic/map/main_2.py
@@ -22,12 +22,20 @@ list_a: list = [
     }
 ]
 
-def hiera_list(list_a: list):
-    print(key)
+
+def hiera_list(list_a: dict):
+    flatten_list = {}
+    for k, v in list_a.items():
+        key = k.lower()
+        val = v['value']
+        flatten_list[key] = val
+    return flatten_list
 
 
 def main():
     data = list(map(hiera_list, list_a))
     return data
 
-if __name__ == '__main__': main()
+
+if __name__ == '__main__':
+    main()
