218b9545dd2f9f10d2ee28b3b04334f24eb27206
inamuu <8310973+inamuu@users.noreply.github.com>
2022-01-11 17:36:47 +0900

    check each value if exists or not

--- _python_basic/keyvalue/check_value.py
+++ _python_basic/keyvalue/check_value.py
@@ -1,7 +1,27 @@
 # coding: utf-8
 
 def main():
-    return
+    test_data = {
+        "name": "Test Taro",
+        "tel": "090-1234-5678",
+        "age": "30",
+        "sex": "male",
+        "address": ""
+    }
+
+    mapping_data = [
+        "name",
+        "tel",
+        "age",
+        "sex",
+        "address"
+    ]
+
+    for k, v in test_data.items():
+        if v != "":
+            print(k + " : " + v)
+        else:
+            print(k + " : None")
 
 
 if __name__ == '__main__':
