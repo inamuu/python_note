e1cf5375d9cc69d08289a8e4864e9386b6305cb7
inamuu <8310973+inamuu@users.noreply.github.com>
2022-10-14 21:35:52 +0900

    Add a function that count local files

--- _python_basic/file_count/main.py
+++ _python_basic/file_count/main.py
@@ -1,7 +1,16 @@
 # coding: utf-8
 
+import os
+
 def main():
-    print('hello')
+    dir = './testdir/'
+    list = os.listdir(dir)
+    i = 0
+    for file in list:
+        index = list.index(file)
+        i = index + 1
+        print(i)
+        return
 
 if __name__ == '__main__': main()
 
